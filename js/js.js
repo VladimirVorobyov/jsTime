883a1c06c5d0052bd130cf87d0eb42bcc8bac367
Vova Vorobiev <vovabandit1945@gmail.com>
2020-11-19 14:00:16 +0300

    lesson18/2

--- js/js.js
+++ js/js.js
@@ -1,45 +1,61 @@
-window.addEventListener('DOMContentLoaded', function(){
-    'use strict';
-    function countTimer (deadline){
-        let timerHours = document.querySelector('#timer-hours'),
-            timerMinutes = document.querySelector('#timer-minutes'),
-            timerSeconds = document.querySelector('#timer-seconds');
+'use strict';
 
-        function getNum (num){
-            if ( num < 10) { 
-                return '0' + num;
-            }else {
-             return num;
-            }
-        }
-        
-        function getTimeRemaining (){
-            let dateStop = new Date(deadline).getTime(),
-                dateNow = new Date().getTime(),
-                timerRemaining = (dateStop - dateNow) / 1000,
-                seconds =  Math.floor(timerRemaining % 60),
-                minutes = Math.floor((timerRemaining / 60) % 60),
-                hours = Math.floor(timerRemaining / 60 / 60);
-                return{ timerRemaining , hours, minutes, seconds};
-        }
-        
-        function updateClock(){
-            let timer = getTimeRemaining();
-            timerHours.textContent = getNum(timer.hours);
-            timerMinutes.textContent = getNum(timer.minutes);
-            timerSeconds.textContent = getNum(timer.seconds);
-            if(timer.timerRemaining > 0){     
-                setInterval(updateClock, 1000);
-               } else {
-                timerHours.textContent = '00';
-                timerMinutes.textContent = '00';
-                timerSeconds.textContent = '00';
-               }
-            
-        }
-        updateClock();    
-       
-
+let period = document.querySelector('#period'),
+    day = document.querySelector('#day'),
+    hours = document.querySelector('#hours'),
+    newYear = document.querySelector('#newYear'),
+    dateNow = new Date(),
+    dateStop = new Date('1 january 2021'),
+    day1 = 'Понедельник',
+    day2 = 'Вторник',
+    day3 = 'Среда',
+    day4 = 'Четверг',
+    day5 = 'Пятница',
+    day6 = 'Суббота',
+    day0= 'Воскресенье';
+    
+function getDayWeek (){
+    if(dateNow.getDay() === 1){
+        day.textContent = 'Сегодня: ' + day1;
+    } else if(dateNow.getDay() === 2){
+        day.textContent = 'Сегодня: ' + day2; 
+    } else if(dateNow.getDay() === 3){
+        day.textContent = 'Сегодня: ' + day3;
+    } else if(dateNow.getDay() === 4){
+        day.textContent = 'Сегодня: ' + day4;
+    } else if(dateNow.getDay() === 5){
+        day.textContent = 'Сегодня: ' + day5;
+    } else if(dateNow.getDay() === 6){
+        day.textContent = 'Сегодня: ' + day6;
+    } else if(dateNow.getDay() === 1){
+        day.textContent = 'Сегодня: ' + day0;
     }
-    countTimer('20 november 2020 12:47:56');
-});
+}     
+
+function getAfternoon (){
+  if(12 >= dateNow.getHours() && dateNow.getHours()>= 6 ){
+   period.textContent = 'Доброе утро';
+  }else if( dateNow.getHours() > 12  && dateNow.getHours() < 18){
+    period.textContent = 'Добрый день';
+  }else if(22 >= dateNow.getHours() && dateNow.getHours() >= 18 ){
+    period.textContent = 'Добрый вечер';
+  }else{
+    period.textContent = 'Доброй ночи';
+  }
+}
+
+function  getNewYear (){
+  let  timerRemaining = (dateStop - dateNow) / 1000, 
+       dayNew1 = Math.floor(timerRemaining / 60 / 60 / 24);
+ newYear.textContent = 'До нового года осталось '+ dayNew1 +' день' ;
+} 
+
+ function getCurrentTime (){
+     hours.textContent = 'Текущее время: ' + dateNow.toLocaleTimeString('en');
+ }
+
+getCurrentTime();
+getDayWeek();
+getAfternoon();
+getNewYear();
+    
